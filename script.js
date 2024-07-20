5c94069a285b266517f9d9c34c62d06c3385e468
Kiookko <166719494+Kiookko@users.noreply.github.com>
2024-07-20 16:44:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const eagle = document.getElementById('eagle');
     const clickCount = document.getElementById('clickCount');
     const resetButton = document.getElementById('resetButton');
-    const customButton = document.getElementById('customButton');
     let count = 0;
 
     eagle.addEventListener('click', () => {
@@ -14,9 +13,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     resetButton.addEventListener('click', () => {
         count = 0;
         clickCount.textContent = count;
-    });
-
-    customButton.addEventListener('click', () => {
-        alert('Кастомная кнопка нажата!');
     });
 });
