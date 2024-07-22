d2b0618fdb0280b05dad076ee568fcdd8ee89f91
Kiookko <166719494+Kiookko@users.noreply.github.com>
2024-07-22 14:56:30 +0500

    Delete hight.js

--- hight.js
+++
@@ -1,4 +0,0 @@
-<script>
- var pageWidth = document.documentElement.clientWidth; //Ширина страницы (документа)
- var pageHeight = document.documentElement.clientHeight; //Высота страницы (документа)
-</script>
